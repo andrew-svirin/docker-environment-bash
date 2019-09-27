1c059668a8b303475f2396737974345dc4b4e5f0
Andrew Svirin <andriy.svirin@ukrinsoft.com>
2019-09-27 13:08:14 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@
 # - Installs docker
 # - Installs docker-compose
 # - Installs git
-# For launch command add current user to sudo group `usermod -aG sudo __CURRENT_USER__`
+# For launch command add current user to sudo group `usermod -aG sudo __CURRENT_USER__` and re-login.
 # Launch by command `wget https://github.com/andrew-svirin/docker-environment-bash/blob/master/setup.sh | sudo bash`
