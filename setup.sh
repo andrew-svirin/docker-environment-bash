6382ce432b71b2e78e6dc0b21a8d8f1d573aa20d
Andrew Svirin <andriy.svirin@ukrinsoft.com>
2019-09-27 13:16:06 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@
 # - Installs docker-compose
 # - Installs git
 # For launch command add current user to sudo group `/usr/sbin/usermod -aG sudo __CURRENT_USER__` and re-login.
-# Launch by command `wget https://github.com/andrew-svirin/docker-environment-bash/blob/master/setup.sh | sudo bash` 
-# or by this short link `wget http://tiny.cc/uz7hdz | sudo bash`
+# Launch by command `wget https://github.com/andrew-svirin/docker-environment-bash/blob/master/setup.sh | /usr/sbin/sudo bash` 
+# or by this short link `wget http://tiny.cc/uz7hdz | /usr/sbin/sudo bash`
 echo "Setup runs ..."
 
 exit
