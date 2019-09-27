be4c98a95c5f64b48afb8605b9f54501ed0d9eac
Andrew Svirin <andriy.svirin@ukrinsoft.com>
2019-09-27 13:23:25 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,9 +9,11 @@
 # For launch command add current user to sudo group `/usr/sbin/usermod -aG sudo __CURRENT_USER__` and re-login.
 # Launch by command `wget https://raw.githubusercontent.com/andrew-svirin/docker-environment-bash/master/setup.sh | /usr/sbin/sudo bash` 
 # or by this short link `wget http://tiny.cc/fb8hdz | bash`
-echo "Setup runs ..."
+echo "Setup runs ...\n"
 
-# Installs ssh server.
+echo "Install openssh-server ...\n"
 sudo apt install openssh-server
+
+
 
 exit
