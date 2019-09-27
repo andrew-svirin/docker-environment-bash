2f11352e5f9a08f3bf1b49ff123a092f3ae7e8fc
Andrew Svirin <andriy.svirin@ukrinsoft.com>
2019-09-27 13:22:13 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # Script installs environment programs for docker handled project in Debian 10 for servers started OS.
-# - Installs ssh
+# - Installs ssh server
 # - Installs Samba with shared `/var/www` folder.
 # - Installs docker
 # - Installs docker-compose
 # - Installs git
 # For launch command add current user to sudo group `/usr/sbin/usermod -aG sudo __CURRENT_USER__` and re-login.
 # Launch by command `wget https://raw.githubusercontent.com/andrew-svirin/docker-environment-bash/master/setup.sh | /usr/sbin/sudo bash` 
-# or by this short link `wget http://tiny.cc/fb8hdz | /usr/sbin/sudo bash`
+# or by this short link `wget http://tiny.cc/fb8hdz | bash`
 echo "Setup runs ..."
+
+# Installs ssh server.
+sudo apt install openssh-server
 
 exit
