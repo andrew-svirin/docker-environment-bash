3c502b4162560969bff80dbeea917f941c562ad8
Andrew Svirin <andriy.svirin@ukrinsoft.com>
2019-09-27 12:56:30 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Script installs environment programs for docker handled project in Debian 10 for servers started OS.
+# - Installs Samba with shared `/var/www` folder.
+# - Installs docker
+# - Installs docker-compose
+# - Installs git
