e9570616dffa95fd8702347225057e924bf1d4e9
Andrew Svirin <andriy.svirin@ukrinsoft.com>
2019-09-27 15:45:28 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -31,15 +31,19 @@ echo "Prepare folder: $WWW_DIR"
 sudo mkdir $WWW_DIR
 sudo chmod 777 $WWW_DIR
 
-echo "Add user group: www-data. Add current user to group www-data"
-sudo groupadd www-data
-sudo /usr/sbin/usermod -aG www-data $USER
+USER_GROUP=www-data
+echo "Add user group: $USER_GROUP. Add current user to group $USER_GROUP"
+sudo groupadd $USER_GROUP
+sudo /usr/sbin/usermod -aG $USER_GROUP $USER
 
 echo -e "Download samba config and setup username \e[0m \e[38;5;198m $USER \e[0m for access dir \e[0m \e[38;5;198m $WWW_DIR \e[0m :"
 SAMBA_CONF_TMP=smb.conf
-sudo wget -q -O - https://raw.githubusercontent.com/andrew-svirin/docker-environment-bash/master/configs/smb.conf | sed -e "s/\$USER/$USER/g" > $SAMBA_CONF_TMP
+sudo wget -q -O - https://raw.githubusercontent.com/andrew-svirin/docker-environment-bash/master/configs/smb.conf | sed -e "s/\$USER/$USER/g" | sed -e "s/\$GROUP/$USER_GROUP/g" > $SAMBA_CONF_TMP
 sudo mv $SAMBA_CONF_TMP $SAMBA_CONF
 
 sudo service smbd restart
+
+IP=`ifconfig | sed -En "s/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p"`
+echo -e "\e[30;48;5;82m samba configured \e[0m Use \e[38;5;198m \\\\\\\\$IP\\\\www \e[0m to mount disk in Windows"
 
 exit
